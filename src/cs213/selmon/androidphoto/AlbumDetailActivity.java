6c54dde73c26088b665d19456e1797352d623a90
David Young-Chan Kay <davidykay@gmail.com>
2012-04-28 19:08:56 -0400

    Added basic activity structure. Added sample photos

--- src/cs213/selmon/androidphoto/AlbumDetailActivity.java
+++ src/cs213/selmon/androidphoto/AlbumDetailActivity.java
@@ -3,13 +3,13 @@ package cs213.selmon.androidphoto;
 import android.app.ListActivity;
 import android.os.Bundle;
 
-public class PhotoActivity extends ListActivity {
+public class AlbumDetailActivity extends ListActivity {
   
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState); 
 
-    setContentView(R.layout.photo);
+    setContentView(R.layout.album_detail);
   }
 
 
--- src/cs213/selmon/androidphoto/AlbumListActivity.java
+++ src/cs213/selmon/androidphoto/AlbumListActivity.java
@@ -1,13 +1,13 @@
 package cs213.selmon.androidphoto;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 
-public class HomeActivity extends Activity {
+public class AlbumListActivity extends ListActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.album_list);
     }
 }
