c69b76df05a8578ea96df2cd49da2d166f3cfb26
David Young-Chan Kay <davidykay@gmail.com>
2012-04-28 20:09:23 -0400

    now creating albums and showing them on the list

--- src/cs213/selmon/androidphoto/AlbumListActivity.java
+++ src/cs213/selmon/androidphoto/AlbumListActivity.java
@@ -1,22 +1,32 @@
 package cs213.selmon.androidphoto;
 
+import java.util.List;
+
 import android.app.ListActivity;
+import android.database.DataSetObserver;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.TextView;
 import cs213.selmon.androidphoto.model.Album;
 import cs213.selmon.androidphoto.util.DataStore;
 
 public class AlbumListActivity extends ListActivity {
 
 
+  private DataStore mDataStore;
+
   /** Called when the activity is first created. */
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.album_list);
 
+    mDataStore = DataStore.getInstance();
+
+    setContentView(R.layout.album_list);
 
     Button addButton = (Button) findViewById(R.id.add_button);
     addButton.setOnClickListener( new View.OnClickListener() {
@@ -24,59 +34,96 @@ public class AlbumListActivity extends ListActivity {
       @Override
       public void onClick(View v) {
         Album newAlbum = new Album("New Name Here");
-        DataStore.getInstance().addAlbum(newAlbum);
+        mDataStore.addAlbum(newAlbum);
+
+        setListAdapter(new AlbumListAdapter(
+            mDataStore.getAlbums()
+            ));
       }
     });
     
-
-     setListAdapter(new ArrayAdapter<String>(this,
-          android.R.layout.simple_list_item_1, COUNTRIES));
-    
+     setListAdapter(new AlbumListAdapter(
+         mDataStore.getAlbums()
+     ));
   }
 
+  private class AlbumListAdapter implements ListAdapter {
 
-  static final String[] COUNTRIES = new String[] {
-    "Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra",
-    "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina",
-    "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan",
-    "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium",
-    "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia",
-    "Bosnia and Herzegovina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory",
-    "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi",
-    "Cote d'Ivoire", "Cambodia", "Cameroon", "Canada", "Cape Verde",
-    "Cayman Islands", "Central African Republic", "Chad", "Chile", "China",
-    "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo",
-    "Cook Islands", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic",
-    "Democratic Republic of the Congo", "Denmark", "Djibouti", "Dominica", "Dominican Republic",
-    "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea",
-    "Estonia", "Ethiopia", "Faeroe Islands", "Falkland Islands", "Fiji", "Finland",
-    "Former Yugoslav Republic of Macedonia", "France", "French Guiana", "French Polynesia",
-    "French Southern Territories", "Gabon", "Georgia", "Germany", "Ghana", "Gibraltar",
-    "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau",
-    "Guyana", "Haiti", "Heard Island and McDonald Islands", "Honduras", "Hong Kong", "Hungary",
-    "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica",
-    "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kuwait", "Kyrgyzstan", "Laos",
-    "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg",
-    "Macau", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands",
-    "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia", "Moldova",
-    "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia",
-    "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand",
-    "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "North Korea", "Northern Marianas",
-    "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru",
-    "Philippines", "Pitcairn Islands", "Poland", "Portugal", "Puerto Rico", "Qatar",
-    "Reunion", "Romania", "Russia", "Rwanda", "Sqo Tome and Principe", "Saint Helena",
-    "Saint Kitts and Nevis", "Saint Lucia", "Saint Pierre and Miquelon",
-    "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Saudi Arabia", "Senegal",
-    "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands",
-    "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "South Korea",
-    "Spain", "Sri Lanka", "Sudan", "Suriname", "Svalbard and Jan Mayen", "Swaziland", "Sweden",
-    "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "The Bahamas",
-    "The Gambia", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey",
-    "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Virgin Islands", "Uganda",
-    "Ukraine", "United Arab Emirates", "United Kingdom",
-    "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan",
-    "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Wallis and Futuna", "Western Sahara",
-    "Yemen", "Yugoslavia", "Zambia", "Zimbabwe"
-  };
+    private List<Album> mAlbums;
+
+    public AlbumListAdapter(List<Album> albums) {
+      mAlbums = albums;
+    }
+
+    @Override
+    public void registerDataSetObserver(DataSetObserver observer) {
+      // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void unregisterDataSetObserver(DataSetObserver observer) {
+      // TODO Auto-generated method stub
+      
+    }
+
+    @Override
+    public int getCount() {
+      return mAlbums.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+      return mAlbums.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+      //return 0;
+      //return position;
+      return mAlbums.get(position).hashCode();
+    }
+
+    @Override
+    public boolean hasStableIds() {
+      return false;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+      LayoutInflater inflater = getLayoutInflater();
+      View row = inflater.inflate(R.layout.album_row, null);
+      TextView text = (TextView) row.findViewById(R.id.text);
+      Album album = mAlbums.get(position);
+      text.setText(album.getName());
+      
+      return(row);
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+      return 1;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+      return 1;
+    }
+
+    @Override
+    public boolean isEmpty() {
+      return getCount() == 0;
+    }
+
+    @Override
+    public boolean areAllItemsEnabled() {      
+      return true;
+    }
+
+    @Override
+    public boolean isEnabled(int position) {
+      return true;
+    }
+
+  }
 
 }
